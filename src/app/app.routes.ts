5df2a26270d8d44df3598fb83a3e2de81c0f3062
raj dreamlogic lenovo <raj44.naik@gmail.com>
2024-04-15 14:50:58 +0530

    baseurl route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { ContactusComponent } from './contactus/contactus.component';
 import { PagenorfoundComponent } from './pagenorfound/pagenorfound.component';
 
 export const routes: Routes = [
+    {path:'',redirectTo:'home',pathMatch:'full'},
     {path:'home', component:DashboardComponent,title:'home page'},
     {path:'aboutus',component:AboutusComponent,title:'about page'},
     {path:'contactus',component:ContactusComponent,title:'contact page'},
